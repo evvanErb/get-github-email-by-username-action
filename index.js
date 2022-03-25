bb32c744c7452a6ee70688b737e86eef68f73632
Evvan Erb <evvan.erb@vertexinc.com>
2022-03-25 13:26:27 -0400

    Switched to API

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const cheerio = require('cheerio');
-import request from 'request';
+import fetch from "node-fetch";
 const core = require('@actions/core');
 const github = require('@actions/github');
+
+function findEmail(jsonData) {
+
+}
 
 try {
   // `username` input defined in action metadata file
   const username = core.getInput('username');
   console.log(`[*] Getting ${username}\'s GitHub email`);
 
-  request({
-        method: 'GET',
-        url: `https://github.com/${username}`
-        }, (err, res, body) => {
+  //fetch user's page
+  fetch(`https://api.github.com/users/${username}/events/public`)
+  .then(function(response) {
 
-    console.log(body);
-    const $ = cheerio.load(body);
-  
-    $('.u-email').each((i, element) => {
-      const email = element.text();
-      console.log(`[*] Found ${username}\'s email: ${email}`)
-      core.setOutput("email", email);
-    });
-  });
+    // When the page is loaded convert it to text
+    return response.text()
+  })
+  .then((apiData) => {
+
+    const position = apiData.search("\"email\": \"");
+
+    const email = apiData.substring(position, (apiData.substring(position + 11).indexOf('\"')));
+
+    console.log(`[*] Found ${username}\'s email: ${email}`)
+    core.setOutput("email", email);
+  })
 
 } catch (error) {
   core.setFailed(error.message);
