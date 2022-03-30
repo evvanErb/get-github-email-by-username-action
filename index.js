825f943cfeddfa33b4c933c865c5da21ebfa0bc8
Evvan Erb <evvan.erb@vertexinc.com>
2022-03-30 13:31:45 -0400

    Added bot account and cheerio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 const request = require('request');
+const cheerio = require('cheerio');
 const core = require('@actions/core');
 
 function findEmailInAPI(apiData) {
@@ -22,38 +23,50 @@ function findEmailInAPI(apiData) {
 }
 
 function findEmailInUserPage(html) {
-  return html;
+
+  let $ = cheerio.load(html);
+
+  let email = $('.u-email');
+
+  return email.text();
 }
 
 try {
-  // `username` input defined in action metadata file
+  //inputs defined in action metadata file
   const usernameForEmail = core.getInput('github-username');
   const usernameForLogin = core.getInput('login-username');
   const passwordForLogin = core.getInput('login-password');
   console.log(`[*] Getting ${usernameForEmail}\'s GitHub email`);
 
-  //try to login to github and get 
+  //try to login to github with provided credentials
   console.log(`[*] Trying to login as ${usernameForLogin} to get ${usernameForEmail}\'s GitHub email`);
   request.post({
     url: 'https://exampleurl.com/login',
     form: {"login":`${usernameForLogin}`, "password":`${passwordForLogin}`}
   }, function(error, response, body){
 
-    //try to get the desired user's page
     let headers = {}
     if (response != null) {
       headers = response.headers;
+      console.log(`[*] ${usernameForLogin} Authentication successful`);
     }
+    else {
+      console.log(`[!] ${usernameForLogin} Authentication failed`);
+    }
+
+    //Get the desired user's page even if login failed
     request.get({
         url:`https://github.com/${usernameForEmail}`,
         header: headers
     },function(error, response, body){
 
-        // The full html of the authenticated page
+        // Search the full html of the page for the email
         const emailUserpage = findEmailInUserPage(body);
 
-        //Could not login to Github, fallback to old method to attempt email retrieval
+        //email not found on page, fallback to old method to attempt email retrieval
         if (emailUserpage == null) {
+
+          console.log(`[*] Falling back to old API retrieval method`);
           //fetch user's page
           fetch(`https://api.github.com/users/${usernameForEmail}/events/public`)
           .then(function(response) {
@@ -63,7 +76,7 @@ try {
           })
           .then((apiData) => {
 
-            const emailAPI = findEmail(apiData);
+            const emailAPI = findEmailInAPI(apiData);
 
             if (emailAPI == null) {
               throw Error('[!!!] Could not find email in API Data');
